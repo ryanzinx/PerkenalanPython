7035adf7952b12d4c1e46732c333e36918d425d3
ryanzinx <92358422+ryanzinx@users.noreply.github.com>
2021-11-01 01:20:35 +0700

    Add files via upload

---
+++ perkenalanpython.py
@@ -0,0 +1,13 @@
+#Mengambil input
+nama = ("Adrian Fadhali Wiratama.")
+namapanggilan = ("Ryan.")
+npm = ("312110329.")
+tempatlahir = ("Bekasi")
+umur = ("19")
+alamat = ("Perum. Mutiara Bekasi Jaya blok L 17 No.7.")
+telepon = ("081324610584.")
+
+#Menampilkan output
+print("Let me introduce myself. My name is",nama,"But you can call me",namapanggilan,"My NPM is",npm,
+      "I was born in",tempatlahir,"and i'm",umur,"years old.","I am very glad if you want to visit my home at",alamat,
+      "So, don't forget to call me before visit me with my number",telepon)
